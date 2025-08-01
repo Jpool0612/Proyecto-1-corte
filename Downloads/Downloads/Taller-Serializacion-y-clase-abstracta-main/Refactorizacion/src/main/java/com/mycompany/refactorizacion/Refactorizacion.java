87817ed5d9a35b4271a46c0ce31218463f0fde2d
Jpool0612 <jtapierosanches@gmail.com>
2025-07-31 20:17:38 -0500

    Refactorización del proyecto usando Herencia, Polimorfismo y Reutilización

---
+++ Downloads/Downloads/Taller-Serializacion-y-clase-abstracta-main/Refactorizacion/src/main/java/com/mycompany/refactorizacion/Refactorizacion.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.refactorizacion;
+
+/**
+ *
+ * @author Jean Pool
+ */
+public class Refactorizacion {
+    public static void main(String[] args) {
+       
+    }
+}
